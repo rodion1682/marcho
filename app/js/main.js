cce623674ce0006abb73eaac0b7530234c6655cd
Rodion <rodiontropkin17@gmail.com>
2021-02-11 18:39:29 +0200

    added media

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,10 @@ $(function(){
     $('.menu__list').toggleClass('menu__list--active');
   });
 
+  $('.shop__filter-btn').on('click', function(){
+    $('.shop__filters').slideToggle();
+  });
+
   $('.footer-top__title').on('click', function(){
     $(this).siblings().slideToggle();
     $(this).toggleClass('footer-top__nav--atcive');
@@ -37,6 +41,14 @@ $(function(){
     draggable: false, 
     arrows: false,
     fade: true,
+    responsive: [
+      {
+        breakpoint: 1051,
+        settings: {
+          draggable: true,
+        }
+      }
+    ]
   });
 
   $('.shop-content__filter-btn').on('click', function(){
@@ -46,10 +58,12 @@ $(function(){
 
   $('.button-list').on('click', function(){
     $('.product-item').addClass('product-item--list');
+    $('.shop-content__inner').addClass('shop-content__nogrid');
   });
 
   $('.button-grid').on('click', function(){
     $('.product-item').removeClass('product-item--list');
+    $('.shop-content__inner').removeClass('shop-content__nogrid');
   });
 
   $('.select-style, .product-one__num').styler();
